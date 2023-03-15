8827d8e3854bfac9dfaa8f2a403d5d07f43e8163
Ariel Ortiz <ariel.ortiz.ramirez@gmail.com>
2023-03-15 18:21:57 +0000

    Modifica primer elemplo usando express

--- 4. Server JavaScript/simple node app/app.js
+++ 4. Server JavaScript/simple node app/app.js
@@ -4,11 +4,33 @@ const app = express();
 const port = 8080;
 const ipAddr = '34.230.161.23';
 
+app.use(express.static(__dirname + '/public'));
+
 app.get('/', (req, res) => {
   res.type('text/plain');
+  res.status(200);
   res.send('hola mundo');
 });
 
+app.get('/hola/:nombre', (req, res) => {
+  let n = req.params.nombre;
+  res.type('text/plain');
+  res.send(`Hola, ${n}.`);
+});
+
+app.get('/prueba', (req, res) => {
+  res.type('text/plain');
+  res.send('Esta es una prueba en formato texto');
+});
+
+// custom 404 page
+app.use((req, res) => {
+  res.type('text/plain');
+  res.status(404);
+  res.send('404 - Not Found');
+});
+
 app.listen(port, () => {
-  console.log('Listo!');
+  console.log(`Express iniciado en http://${ipAddr}:${port}`
+    + '\nPresiona Ctrl-C para terminar.');
 });
